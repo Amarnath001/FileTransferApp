01c48892688ed46a98464ad8c337e67da46f97c2
Amarnath SK <amarsk99@gmail.com>
2023-05-03 10:44:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/filetransferapp/BluetoothTransfer.java
@@ -0,0 +1,15 @@
+package com.example.filetransferapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BluetoothTransfer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_transfer);
+    }
+
+}
