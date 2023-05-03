b9843426d662ffccf6e015aa4972a460a9797966
Amarnath SK <amarsk99@gmail.com>
2023-05-03 11:05:56 +0530

    Added Receive Activity

---
+++ app/src/main/java/com/example/filetransferapp/RecieveFiles.java
@@ -0,0 +1,14 @@
+package com.example.filetransferapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecieveFiles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recieve_files);
+    }
+}
