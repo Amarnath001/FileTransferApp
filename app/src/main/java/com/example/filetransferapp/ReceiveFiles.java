449b1081d1c126ee9f6490508fff2edea252cc24
Richa710 <rrsinha710@gmail.com>
2023-05-03 11:43:23 +0530

    first commit

--- app/src/main/java/com/example/filetransferapp/ReceiveFiles.java
+++ app/src/main/java/com/example/filetransferapp/ReceiveFiles.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RecieveFiles extends AppCompatActivity {
+public class ReceiveFiles extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
