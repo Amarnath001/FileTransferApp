200a869f9f042c39f850bd0810b32ebcf5154783
lipsabehera-1928 <lipsabehera2328@gmail.com>
2023-05-03 11:52:26 +0530

    First commit

--- app/src/main/java/com/example/filetransferapp/NetworkShare.java
+++ app/src/main/java/com/example/filetransferapp/NetworkShare.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class NetworkShare extends AppCompatActivity {
+    //File share over wifi or mobile data
+    //ewguyewufheuwhf
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
