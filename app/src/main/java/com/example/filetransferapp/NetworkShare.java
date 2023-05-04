f7ff9df97d7ae7c7edfb2da4b0cfab11e4bdc928
lipsabehera-1928 <lipsabehera2328@gmail.com>
2023-05-04 17:59:07 +0530

    Added CheckPermission in NetworkShare file.

--- app/src/main/java/com/example/filetransferapp/NetworkShare.java
+++ app/src/main/java/com/example/filetransferapp/NetworkShare.java
@@ -1,15 +1,20 @@
 package com.example.filetransferapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.text.format.Formatter;
+import android.webkit.PermissionRequest;
 import android.widget.TextView;
 
 public class NetworkShare extends AppCompatActivity {
     String ip;
     TextView Textview;
+    private int PERMISSION_REQUEST_CODE;
 
 
     @Override
@@ -20,5 +25,21 @@ public class NetworkShare extends AppCompatActivity {
         WifiManager manager=(WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         ip= Formatter.formatIpAddress(manager.getConnectionInfo().getIpAddress());
         Textview.setText("Ip Address: "+ ip);
+    }
+
+    private boolean checkPermission(String[] Permission){
+        for(int i=0;i<Permission.length;i++){
+            int result= ContextCompat.checkSelfPermission(NetworkShare.this,Permission[i]);
+            if(result== PackageManager.PERMISSION_GRANTED){
+                continue;
+            }
+            else {
+                return false;
+            }
+        }
+        return  true;
+    }
+    private  void requestPermission(String[] Permission){
+        ActivityCompat.requestPermissions(NetworkShare.this,Permission, PERMISSION_REQUEST_CODE);
     }
 }
