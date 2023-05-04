41381b0e6fb0b978a99284839807b79cfcbcefa8
lipsabehera-1928 <lipsabehera2328@gmail.com>
2023-05-04 16:09:18 +0530

    Added How to get an Ip and changes im Xml file

--- app/src/main/java/com/example/filetransferapp/NetworkShare.java
+++ app/src/main/java/com/example/filetransferapp/NetworkShare.java
@@ -6,23 +6,33 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.text.format.Formatter;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.nio.channels.Channel;
 import java.util.ArrayList;
 
 public class NetworkShare extends AppCompatActivity {
+    String ip;
+    TextView Textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Textview=findViewById(R.id.textView);
+        WifiManager manager=(WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        ip= Formatter.formatIpAddress(manager.getConnectionInfo().getIpAddress());
+        Textview.setText("Ip Address: "+ ip);
+
     }
 }
 
