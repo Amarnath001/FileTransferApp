e3ecd6553abe557e278b0302d2467f4c1208eda0
lipsabehera-1928 <lipsabehera2328@gmail.com>
2023-05-04 15:55:41 +0530

    Changes on NetworkShare file.

--- app/src/main/java/com/example/filetransferapp/NetworkShare.java
+++ app/src/main/java/com/example/filetransferapp/NetworkShare.java
@@ -2,15 +2,27 @@ package com.example.filetransferapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.nio.channels.Channel;
+import java.util.ArrayList;
 
 public class NetworkShare extends AppCompatActivity {
-    //File share over wifi or mobile data
-    //ewguyewufheuwhf
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_network_share);
+        setContentView(R.layout.activity_main);
     }
 }
+
