2454f8802833297eac0f7bab6afe5c41e0b5fdbf
Amarnath SK <amarsk99@gmail.com>
2023-05-05 13:35:25 +0530

    Made it to work with Local network (Ethernet) updated NetworkShare.java

--- app/src/main/java/com/example/filetransferapp/NetworkShare.java
+++ app/src/main/java/com/example/filetransferapp/NetworkShare.java
@@ -17,11 +17,17 @@ import android.webkit.PermissionRequest;
 import android.widget.Button;
 import android.widget.TextView;
 import java.io.File;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class NetworkShare extends AppCompatActivity {
     int reqcode =1;
     Uri uri;
-    String ip;
+    String Wifiip;
+    String NetIp;
     int totalFileSize = 0 ;
     private static final String TAG = "NetworkShare";
     //private int PERMISSION_REQUEST_CODE;
@@ -33,8 +39,14 @@ public class NetworkShare extends AppCompatActivity {
         TextView IpAddress = findViewById(R.id.ipName);
         WifiManager manager=(WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         //noinspection deprecation
-        ip= Formatter.formatIpAddress(manager.getConnectionInfo().getIpAddress());
-        IpAddress.setText("Ip Address: "+ ip);
+        Wifiip= Formatter.formatIpAddress(manager.getConnectionInfo().getIpAddress());
+        if(!Wifiip.equals("0.0.0.0"))
+            IpAddress.setText("Ip Address: "+ Wifiip);
+        else
+        {
+            NetIp = getIpAddress();
+            IpAddress.setText("Ip Address: "+ NetIp);
+        }
         Button getFile = findViewById(R.id.button_GetFile);
         getFile.setOnClickListener(new View.OnClickListener() {
             @SuppressWarnings("deprecation")
@@ -47,10 +59,30 @@ public class NetworkShare extends AppCompatActivity {
             }
         });
     }
+    public static String getIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+
+
     @Override
     public void onActivityResult(int reqcode, int resultcode, Intent data)
     {
-        //TextView FileName = findViewById(R.id.fileName);
+        setContentView(R.layout.activity_network_share);
+        TextView FileName= findViewById(R.id.fileName);
         super.onActivityResult(reqcode, resultcode, data);
         if(resultcode == Activity.RESULT_OK)
         {
@@ -69,7 +101,7 @@ public class NetworkShare extends AppCompatActivity {
                 }
                 Log.d(TAG,"The File Names Are : "+fileNames);
                 Log.d(TAG,"The size of files are : "+totalFileSize);
-          //      FileName.setText(fileNames);
+                FileName.setText(fileNames);
             }
             else
             {
