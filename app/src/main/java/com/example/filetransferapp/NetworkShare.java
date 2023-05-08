5247d4bd3f2263915c4b3ed94e992aff2d8e02e3
Amarnath SK <amarsk99@gmail.com>
2023-05-08 12:16:36 +0530

    Added Socket send to sharing, Yet to test with transfer

--- app/src/main/java/com/example/filetransferapp/NetworkShare.java
+++ app/src/main/java/com/example/filetransferapp/NetworkShare.java
@@ -11,25 +11,135 @@ import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.format.Formatter;
 import android.util.Log;
 import android.view.View;
 import android.webkit.PermissionRequest;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
+import java.nio.BufferUnderflowException;
 import java.util.Enumeration;
 
 public class NetworkShare extends AppCompatActivity {
     int reqcode =1;
     String Wifiip;
     String NetIp;
+    static final int SocketServerPORT = 8080;
+    ServerSocket serverSocket;
+    FileTxThread fileTxThread;
+    serverSocketThread ServerSocketThread;
+    //ServerSocketThread serverSocketThread;
     private static final String TAG = "NetworkShare";
+
+    public void SendFile(View view) {
+        ServerSocketThread = new serverSocketThread();
+        Log.v(TAG,"In sendfile button");
+        ServerSocketThread.start();
+
+    }
+
     //private int PERMISSION_REQUEST_CODE;
+    public class serverSocketThread extends Thread {
+
+        public void run() {
+            Socket socket = null;
+            try{
+                Log.v(TAG,"IN socket thread run!!!");
+                serverSocket = new ServerSocket(SocketServerPORT);
+                NetworkShare.this.runOnUiThread(new Runnable() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void run() {
+                        setContentView(R.layout.activity_network_share);
+                        TextView infoport = findViewById(R.id.infoport);
+                        infoport.setText("Waiting at : "+serverSocket.getLocalPort());
+                    }
+                });
+                while(true){
+                    Log.v(TAG,"In socket thread while loop");
+                    socket = serverSocket.accept();
+                    fileTxThread = new FileTxThread(socket);
+                    fileTxThread.start();
+                }
+            }catch (IOException e)
+            {
+                e.printStackTrace();
+            }finally {
+                Log.v(TAG,"In socket thread finally section");
+                if(socket!=null)
+                {
+                    try {
+                        socket.close();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    public class FileTxThread extends Thread{
+        Uri uri;
+        Socket socket;
+        FileTxThread(Socket socket)
+        {
+            Log.v(TAG,"Got socket info: "+socket);
+            this.socket=socket;
+        }
+
+        public void setUri(Uri uri) {
+            Log.v(TAG,"URI is : "+uri.getPath());
+            this.uri = uri;
+        }
+
+        @Override
+        public void run() {
+            Log.v(TAG,"In FileTXthread run!!!");
+            File file = new File(uri.getPath());
+            Log.v(TAG,"File is : "+file);
+            byte[] bytes = new byte[(int)file.length()];
+            BufferedInputStream bis;
+            try {
+                bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytes,0,bytes.length);
+                OutputStream os = socket.getOutputStream();
+                os.write(bytes,0,bytes.length);
+                os.flush();
+                socket.close();
+
+                final String sentMsg = "File Sent to : " + socket.getInetAddress();
+                NetworkShare.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(NetworkShare.this,sentMsg,Toast.LENGTH_LONG).show();
+                    }
+                });
+            }catch (IOException e)
+            {
+                e.printStackTrace();
+            }finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +155,18 @@ public class NetworkShare extends AppCompatActivity {
             NetIp = getIpAddress();
             IpAddress.setText("Ip Address: " + NetIp);
         }
-        Button getFile = findViewById(R.id.button_GetFile);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
     public static String getIpAddress() {
         try {
@@ -93,9 +214,11 @@ public class NetworkShare extends AppCompatActivity {
                 for(int i=0; i<data.getClipData().getItemCount();i++)
                 {
                    Uri uri = data.getClipData().getItemAt(i).getUri();
+                   fileTxThread.setUri(uri);
                    try{
                     AssetFileDescriptor fileDescriptor = getApplicationContext().getContentResolver().openAssetFileDescriptor(uri , "r");
-                    totalFileSize+=fileDescriptor.getLength();}
+                    totalFileSize+=fileDescriptor.getLength();
+                   }
                    catch(Exception e)
                    {
                        Log.d(TAG,"ERROR IN ONACTIVITY RESULT");
